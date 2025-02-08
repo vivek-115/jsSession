e38207f9f9ddbafef2e56946a646218cf5bab9d8
vivek-115 <78072661+vivek-115@users.noreply.github.com>
2025-02-08 08:52:58 +0000

    Sum of array without Flat

---
+++ recursionArray.js
@@ -0,0 +1,18 @@
+let arr=[1,2,[3,1,[4,2],5],7,8];
+
+function sumArray(arr){
+    let sum=0;
+
+    for(let i=0;i<arr.length;i++){
+        let element=arr[i];
+
+        if(Array.isArray(element)){
+            sum+=sumArray(element);
+        }else{
+            sum+=element
+        }
+    }
+    return sum;
+}
+
+console.log("The Sum is: "+sumArray(arr));
